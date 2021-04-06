81ada5ed2051983388644528ea3fe81227126f49
lcm97 <linchunmin1997@gmail.com>
2021-04-06 14:03:37 +0800

    wxpay

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -6,11 +6,15 @@ export function isExternal(path) {
     return /^(https?:|mailto:|tel:)/.test(path)
 }
 
-/**
- * @param {string} str
- * @returns {Boolean}
- */
+// /**
+//  * @param {string} str
+//  * @returns {Boolean}
+//  */
+// export function validUsername(str) {
+//     const valid_map = ['admin', 'editor']
+//     return valid_map.indexOf(str.trim()) >= 0
+// }
+
 export function validUsername(str) {
-    const valid_map = ['admin', 'editor']
-    return valid_map.indexOf(str.trim()) >= 0
+    return true
 }
